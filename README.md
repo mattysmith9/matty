c879f08fd3801ede1393a7e5a6cfe170c6e20b5f
Matty Smith <matthewcsmith18@gmail.com>
2019-01-24 20:07:43 -0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# My Personal Portfolio Page
-
-A single page app to showcase a couple of projects and tell you more about myself.
-
-The project was bootstrapped with Create React App and deployed with gh-pages.
-
-It is still a work in progress....
---
+++ precache-manifest.280578548cd38c5963341359cc02788c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7727a1d69a4d7612e514",
+    "url": "/static/js/main.7727a1d6.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "7727a1d69a4d7612e514",
+    "url": "/static/css/main.ad394897.chunk.css"
+  },
+  {
+    "revision": "4f12d2db9fce40e9805962ad9e28dfeb",
+    "url": "/index.html"
+  }
+];
