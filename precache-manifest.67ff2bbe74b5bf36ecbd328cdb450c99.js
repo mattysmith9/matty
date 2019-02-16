fcfcf5f971b15043da9710c1be32a99fee9f50de
Matty Smith <matthewcsmith18@gmail.com>
2019-02-16 13:04:01 -0500

    Updates

---
+++ precache-manifest.67ff2bbe74b5bf36ecbd328cdb450c99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5d2cfc91661cd77ebad",
+    "url": "/static/css/main.71f91deb.chunk.css"
+  },
+  {
+    "revision": "a5d2cfc91661cd77ebad",
+    "url": "/static/js/main.a5d2cfc9.chunk.js"
+  },
+  {
+    "revision": "11dac3cdf202915bf55a",
+    "url": "/static/js/1.11dac3cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "280ba81c56f5d38c969d515b839e3b48",
+    "url": "/index.html"
+  }
+];
